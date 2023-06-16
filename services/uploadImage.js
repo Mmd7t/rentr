56b0e025fa3d62227ceeaf0bca60ec884253e13d
Medhat Mostafa <mmedhatmostafa3@gmail.com>
2023-06-16 21:54:13 +0300

    fix bugs

--- services/uploadImage.js
+++ services/uploadImage.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, './')
+        cb(null, './images/')
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname))
